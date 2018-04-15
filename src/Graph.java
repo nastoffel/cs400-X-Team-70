d3e2e058964190b2c73e5c6c23e4fb14cd3c8de7
snoberger <36823018+snoberger@users.noreply.github.com>
2018-04-15 12:22:43 -0500

    Update Graph.java
    finished all methods except remove vertex still throws a concurrent modification error. also completely commented

--- src/Graph.java
+++ src/Graph.java
@@ -1,6 +1,7 @@
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.HashSet;
+import java.util.Iterator;
 import java.util.Set;
 
 /**
@@ -28,13 +29,16 @@ public class Graph<E> implements GraphADT<E> {
 	 */
 	@Override
 	public E addVertex(E vertex) {
+		// check for null
 		if (vertex == null)
 			return null;
+		// check for the vertex to already be in the graph
 		for (GraphNode<E> e : graph) {
 			if (e.getVertex().equals(vertex)) {
 				return null;
 			}
 		}
+		// if not add the vertex and return it
 		graph.add(new GraphNode<E>(vertex));
 		return vertex;
 	}
@@ -44,14 +48,19 @@ public class Graph<E> implements GraphADT<E> {
 	 */
 	@Override
 	public E removeVertex(E vertex) {
+		// check for a null vertex
 		if (vertex == null)
 			return null;
 		GraphNode<E> node = null;
+		// if there exists a node with the specified vertex set that node to the
+		// temporary one
 		for (GraphNode<E> e : graph) {
 			if (e.getVertex().equals(vertex)) {
 				node = e;
 			}
 		}
+		// if a node was found then go through and deletes the edges from all of its
+		// neighbors
 		if (node != null) {
 			for (E neighbor : getNeighbors(node.getVertex()))
 				removeEdge(node.getVertex(), neighbor);
@@ -68,23 +77,25 @@ public class Graph<E> implements GraphADT<E> {
 	public boolean addEdge(E vertex1, E vertex2) {
 		try {
 			boolean added = false;
+			// check if both vertices are in the graph
 			boolean contained = false;
-			if (vertex1.equals(vertex2)) {
+			if (vertex1.equals(vertex2) || vertex1 == null || vertex2 == null) {
 				return false;
 			}
 			for (GraphNode<E> e : graph) {
 				if (e.getVertex().equals(vertex1)) {
 					contained = true;
 				}
-				if(contained)
-				for (GraphNode<E> n : graph) {
-					if (n.getVertex().equals(vertex2)) {
-						contained = true;
-						break;
-					} else
-						contained = false;
-				}
+				if (contained)
+					for (GraphNode<E> n : graph) {
+						if (n.getVertex().equals(vertex2)) {
+							contained = true;
+							break;
+						} else
+							contained = false;
+					}
 			}
+			// if they are contained then add edges going from each vertex to the other
 			if (contained) {
 				for (GraphNode<E> e : graph) {
 
@@ -108,37 +119,39 @@ public class Graph<E> implements GraphADT<E> {
 	 */
 	@Override
 	public boolean removeEdge(E vertex1, E vertex2) {
-		boolean added = false;
+		boolean removed = false;
+		// check if there exists both vertices to be removed
 		boolean contained = false;
-		if (vertex1.equals(vertex2)) {
+		if (vertex1.equals(vertex2) || vertex1 == null || vertex2 == null) {
 			return false;
 		}
 		for (GraphNode<E> e : graph) {
 			if (e.getVertex().equals(vertex1)) {
 				contained = true;
 			}
-			if(contained)
-			for (GraphNode<E> n : graph) {
-				if (n.getVertex().equals(vertex2)) {
-					contained = true;
-					break;
-				} else
-					contained = false;
-			}
+			if (contained)
+				for (GraphNode<E> n : graph) {
+					if (n.getVertex().equals(vertex2)) {
+						contained = true;
+						break;
+					} else
+						contained = false;
+				}
 		}
+		// if they are contained then remove all edges between them
 		if (contained) {
 			for (GraphNode<E> e : graph) {
-				if (e.equals(vertex1)) {
+				if (e.getVertex().equals(vertex1) && e.getEdges().contains(vertex2)) {
 					e.removeEdge(vertex2);
-				}
-				if (e.equals(vertex2)) {
+				} else if (e.getVertex().equals(vertex2) && e.getEdges().contains(vertex1)) {
 					e.removeEdge(vertex1);
 				}
 			}
-			added = true;
+			// set the remove boolean to true
+			removed = true;
 		}
-
-		return added;
+		// return the result
+		return removed;
 	}
 
 	/**
@@ -147,23 +160,25 @@ public class Graph<E> implements GraphADT<E> {
 	@Override
 	public boolean isAdjacent(E vertex1, E vertex2) {
 		try {
+			// check if both vertices are contained non-null and different
 			boolean contained = false;
-			if (vertex1.equals(vertex2)) {
+			if (vertex1.equals(vertex2) || vertex1 == null || vertex2 == null) {
 				return false;
 			}
 			for (GraphNode<E> e : graph) {
 				if (e.getVertex().equals(vertex1)) {
 					contained = true;
 				}
-				if(contained)
-				for (GraphNode<E> n : graph) {
-					if (n.getVertex().equals(vertex2)) {
-						contained = true;
-						break;
-					} else
-						contained = false;
-				}
+				if (contained)
+					for (GraphNode<E> n : graph) {
+						if (n.getVertex().equals(vertex2)) {
+							contained = true;
+							break;
+						} else
+							contained = false;
+					}
 			}
+			// if they are in the graph then check if they have an edge between them
 			if (contained) {
 				for (GraphNode<E> e : graph)
 					if (e.getVertex().equals(vertex1))
@@ -182,10 +197,13 @@ public class Graph<E> implements GraphADT<E> {
 	 */
 	@Override
 	public Iterable<E> getNeighbors(E vertex) {
+		// check for null
 		if (vertex == null)
 			return null;
 		try {
+			// find the vertex
 			for (GraphNode<E> e : graph)
+				// once found, return its adjacency list
 				if (e.getVertex().equals(vertex))
 					return e.getEdges();
 			return null;
@@ -199,29 +217,34 @@ public class Graph<E> implements GraphADT<E> {
 	 */
 	@Override
 	public Iterable<E> getAllVertices() {
+		// create an array list
 		ArrayList<E> vertices = new ArrayList<E>();
+		// for each node add its vertex to the array list
 		for (GraphNode<E> e : graph)
 			vertices.add(e.getVertex());
 		return vertices;
 	}
 
-	private boolean hasVertex(E vertex) {
-		for (GraphNode<E> e : graph)
-			if (e.getVertex().equals(vertex))
-				return true;
-		return false;
-	}
 }
 
 class GraphNode<T> {
 	private T vertex;
 	private ArrayList<T> adjacencyList;
 
+	/**
+	 * constructs an empty GraphNode
+	 */
 	public GraphNode() {
 		vertex = null;
 		adjacencyList = new ArrayList<T>();
 	}
 
+	/**
+	 * constructs a Graphnode with a specified vertex
+	 * 
+	 * @param vertex
+	 *            the vertex of the new node
+	 */
 	public GraphNode(T vertex) {
 		this.vertex = vertex;
 		adjacencyList = new ArrayList<T>();
@@ -231,6 +254,13 @@ class GraphNode<T> {
 		return vertex;
 	}
 
+	/**
+	 * adds an edge between this node and another vertex
+	 * 
+	 * @param vertex
+	 *            the destination of the edge
+	 * @return the vertex passed in if an edge is created
+	 */
 	public T addEdge(T vertex) {
 		if (adjacencyList.contains(vertex))
 			return null;
@@ -238,6 +268,13 @@ class GraphNode<T> {
 		return vertex;
 	}
 
+	/**
+	 * removes an edge between this node and the vertex passed in
+	 * 
+	 * @param vertex
+	 *            the destination of the edge to be removed
+	 * @return the vertx of the destination of the target if it is removed
+	 */
 	public T removeEdge(T vertex) {
 		if (adjacencyList.contains(vertex)) {
 			adjacencyList.remove(vertex);
