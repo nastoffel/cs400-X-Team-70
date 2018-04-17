527cb355a3bd3fd4039885a2179bd404561ab5de
joshstoecker <36823034+joshstoecker@users.noreply.github.com>
2018-04-16 20:51:29 -0500

    Update WordProcessor.java

--- src/WordProcessor.java
+++ src/WordProcessor.java
@@ -1,11 +1,7 @@
-import java.io.FileReader;
 import java.io.IOException;
 import java.nio.file.Files;
-import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.ArrayList;
-import java.util.Collection;
-import java.util.function.Function;
 import java.util.stream.Stream;
 
 /**
